a1ac9ad86ae424a5fe4bbe6e1cc5a017bde214b6
Zhogin Ivan <mr.i-zhogin@yandex.ru>
2022-12-29 14:31:14 +0300

    func in main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,55 @@
+//В прошлый раз мы рассматривали упрощённый вариант вычисления комиссии. Давайте усложним задачу.
+//
+//За MasterCard и Maestro вообще не нужно платить, пока не превысили лимит (замечание от 300 убираем), а для VK Pay всегда бесплатно:
+//Напишите алгоритм расчёта в виде функции, передавая в функцию:
+//
+//тип карты/счёта (по умолчанию VK Pay);
+//сумму предыдущих переводов в этом месяце (по умолчанию 0 рублей);
+//сумму совершаемого перевода.
 import java.util.*
-
 fun main() {
 
-    val fee: Float = 0.75f // размер комиссии
-    val transferMin: UInt = 3500u // минимальный размер платежа в копейках
+
     val amount = Scanner(System.`in`).useLocale(Locale.US) // использую точку как delimiter, а не запятую
+    // лимиты в копейках
+    val dayCardLimitTransfer = 150_000_00
+    val monthCardLimitTransfer = 600_000_00
+    val beforeFee = 75_000_00
+    val vkPerOnceLimit = 15_000_00
+    val vkPerMonthLimit = 40_000_00
+
+    fun calculate(typeCard :String = "VK Pay", curentMonthTrasfer: Double = 0.0, amountTransfer : Double): String = when {
+        (typeCard == "MasterCard" || typeCard =="Maestro") -> when {
+            (amountTransfer < dayCardLimitTransfer && amountTransfer < beforeFee && curentMonthTrasfer < monthCardLimitTransfer &&
+                    (amountTransfer + curentMonthTrasfer) < monthCardLimitTransfer) -> "сумма перевода составит $amountTransfer без комиссии"
+            (amountTransfer < dayCardLimitTransfer && amountTransfer > beforeFee && curentMonthTrasfer < monthCardLimitTransfer &&
+                    (amountTransfer + curentMonthTrasfer) < monthCardLimitTransfer) -> "сумма перевода составит - ${(amountTransfer * 1.006 + 2000)/100 }руб, комиссия - " +
+                    "${(amountTransfer * 0.06 + 2000)/100} руб"
+            else -> "Вы превысили лимиты! Перевод не осуществлён!"
+        }
+        (typeCard == "Visa"|| typeCard == "Мир") -> when {
+            (amountTransfer < dayCardLimitTransfer && curentMonthTrasfer < monthCardLimitTransfer &&
+                    (amountTransfer + curentMonthTrasfer) < monthCardLimitTransfer) -> when {
+                        (amountTransfer * 0.0075) < 3500 -> "сумма перевода составит - ${(amountTransfer + 3500) / 100}руб, комиссия - 35 руб"
+                        else -> "сумма перевода составит - ${(amountTransfer * 1.0075) / 100}руб, комиссия - ${(amountTransfer * 0.0075)/100} руб"
+            }
+            else -> "Вы превысили лимиты! Перевод не осуществлён!"
+
+        }
+        else -> when {
+            (amountTransfer < vkPerOnceLimit && curentMonthTrasfer < vkPerMonthLimit && (amountTransfer + curentMonthTrasfer) < vkPerMonthLimit) ->
+                "сумма перевода составит $amountTransfer без комиссии"
+            else -> "Вы превысили лимиты! Перевод не осуществлён!"
+        }
+
+
+    }
+
 
     while (true){
-        println("Введите сумму перевода в рублях. Минимальный размер перевода составляет ${transferMin/100u} рублей.")
-        val userInput = (amount.nextDouble()*100).toUInt(); // сразу перевожу в копейки.
-        if (userInput < transferMin) { // проверка на мин платеж
-            println("Минимальный размер перевода составляет ${transferMin / 100u}\"!")
-            continue;
-        } else {
-            val amountFee = userInput.toInt() * fee / 100
-            println("Комиссия за перевод в ${userInput.toDouble() /100} рублей составит: ${amountFee /100} руб.")
-            break
-        }
+        println("Введите сумму перевода в рублях: ")
+        val userInput = (amount.nextDouble()*100); // сразу перевожу в копейки.
+        println(calculate(amountTransfer = userInput))
 
     }
 
